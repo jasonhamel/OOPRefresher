a48202954d1e62cf20e8410d2d53b37d97012804
Jason <github@jasongetsemail.com>
2023-10-02 19:53:41 -0400

    First lesson completed

---
+++ Java Inheritance 1/src/Bird.java
@@ -0,0 +1,17 @@
+public class Bird extends Animal{
+    void fly() {
+        System.out.println("I am flying");
+    }
+
+    /*
+    Challenge - add a sing method to the Bird class, then modify the main method accordingly so that the code prints the following lines:
+
+    I am walking
+    I am flying
+    I am singing
+
+     */
+    void sing() {
+        System.out.println("I am singing");
+    }
+}
