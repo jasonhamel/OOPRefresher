0150f2b3bdd1589c2bc0902894febb76135f1805
Jason <github@jasongetsemail.com>
2023-10-02 20:46:51 -0400

    Completed the divisor_sum method

--- Java Interface/src/MyCalculator.java
+++ Java Interface/src/MyCalculator.java
@@ -1,6 +1,12 @@
-public class MyCalculator implements AdvancedArithmetic {
+class MyCalculator implements AdvancedArithmetic {
     @Override
     public int divisor_sum(int n) {
-        return 0;
+        int divisorSum = 0;
+        for (int i = 1; i < n + 1; i++) {
+            if (n % i == 0) {
+                divisorSum += i;
+            }
+        }
+        return divisorSum;
     }
 }
